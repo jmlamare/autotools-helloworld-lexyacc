f2f203e0c11f213aed77acd521fdaa9dda2ab14a
jmlamare <nobody@nowhere.com>
2016-02-21 23:09:15 +0100

    First commit

--- expressions/make.sh
+++ expressions/make.sh
@@ -2,5 +2,5 @@ yacc -d expressions.parser.y  -b expressions.parser
 mv     expressions.parser.tab.h expressions-expressions.parser.h 
 mv     expressions.parser.tab.c expressions-expressions.parser.c 
 lex -o expressions-expressions.lexer.c expressions.lexer.l
-gcc -o expressions expressions-expressions.parser.c expressions-expressions.lexer.c -ly -ll
+gcc -o expressions expressions-expressions.parser.c expressions-expressions.lexer.c -lm -ll
 
